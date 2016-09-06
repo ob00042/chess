b1e16038dcc8d8a98817c195867b3490d47ee3cb
ob00042 <ob00042@gmail.com>
2016-09-06 18:56:09 +0300

    Work on board

--- chess/lib/chess/board.rb
+++ chess/lib/chess/board.rb
@@ -2,10 +2,24 @@ module Chess
 
 	class Board
 
-		attr_accessor :board
-		
-		def initialize
-			board = Array.new(8){ Array.new(8) { Cell.new } }
+		attr_reader :grid
+
+		def initialize(input = {})
+			@grid = input.fetch(:grid, default_grid)
+		end
+
+		def get_cell(x, y)
+			grid[y][x]
+		end
+
+		def set_cell(x, y, value)
+			get_cell(x, y).value = value
+		end
+
+		private
+
+		def default_grid
+			Array.new(8) { Array.new(8) { Cell.new } }
 		end
 
 
--- chess/spec/board_spec.rb
+++ chess/spec/board_spec.rb
@@ -4,17 +4,50 @@ module Chess
 
 	describe Board do 
 
-		before :each do
-			@board = Board.new
-		end
-
 		context "#initialize" do
 
-			it "creates an 8x8 array" do
-				expect(@board.row).to eq 8
-				expect(@board.column).to eq 8
+			it "initializes the board with a grid" do
+				expect{ Board.new(grid: "grid") }.to_not raise_error
 			end
 
+			it "creates a grid with 8 rows" do
+				board = Board.new
+				expect(board.grid.size).to eq 8
+			end
+
+			it "creates eight things in each row" do
+				board = Board.new
+				board.grid.each do |row|
+					expect(row.size).to eq 8
+				end
+			end
+
+			it "creates a grid with Cell objects" do
+				board = Board.new
+				board.grid.each do |row|
+					row.each do |obj|
+						expect(obj).to be_kind_of(Cell)
+					end 
+				end
+			end
+		end
+
+		context "#get_cell" do
+			it "returns the cell based on (x,y) coordinates" do
+				grid = [["","","","","","","",""], ["","","something","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""]]
+				board = Board.new(grid: grid)
+				expect(board.get_cell(2, 1)).to eq "something"
+			end
+		end
+
+		context "#set_cell" do
+			it "updates the value of the cell object at a (x,y) coordinate" do
+				Cat = Struct.new(:value)
+				grid = [[Cat.new("cool"),"","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""], ["","","","","","","",""]]
+				board = Board.new(grid: grid)
+				board.set_cell(0, 0, "meow")
+				expect(board.get_cell(0,0).value).to eq "meow"
+			end
 		end
 
 	end
