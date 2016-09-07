378052ff4cb748cb7b4a8d996ac423f71546544e
ob00042 <ob00042@gmail.com>
2016-09-07 18:50:19 +0300

    Start pieces and player association

--- chess/lib/chess/cell.rb
+++ chess/lib/chess/cell.rb
@@ -2,17 +2,29 @@ module Chess
 
 	class Cell
 
-		attr_accessor :value, :color
+		attr_accessor :value, :color, :player
 
 		def initialize(value = "")
 			@value = value
+			@player = :empty
 		end
 
-		def cell_color(color)
-			if color == :white || color == :black
-				@color = color
+		#IRRELEVANT
+		# def cell_color(color)
+		# 	if color == :white || color == :black
+		# 		@color = color
+		# 	else
+		# 		puts "Please enter a color either :white or :black"
+		# 	end
+		# end
+
+		def cell_player
+			if self.value == :WP1 || self.value == :WP2 || self.value == :WP3 || self.value == :WP4 || self.value == :WP5 || self.value == :WP6 || self.value == :WP7 || self.value == :WP8 || self.value == :WR1 || self.value == :WK1 || self.value == :WB1 || self.value == :WG || self.value == :WQ || self.value == :WB2 || self.value == :WK2 || self.value == :WR2
+				self.player = :player_white
+			elsif self.value == :BP1 || self.value == :BP2 || self.value == :BP3 || self.value == :BP4 || self.value == :BP5 || self.value == :BP6 || self.value == :BP7 || self.value == :BP8 || self.value == :BR1 || self.value == :BK1 || self.value == :BB1 || self.value == :BG || self.value == :BQ || self.value == :BB2 || self.value == :BK2 || self.value == :BR2
+				self.player = :player_black
 			else
-				puts "Please enter a color either :white or :black"
+				self.player = :empty
 			end
 		end
 
--- chess/spec/cell_spec.rb
+++ chess/spec/cell_spec.rb
@@ -13,17 +13,221 @@ module Chess
 			end
 		end
 
-		context "#cell_color" do
-			it "has white color if :white is given" do
-				allow(@cell.cell_color(:white))
-				expect(@cell.color).to eq :white
+		# IRRELEVANT!!!!
+		# context "#cell_color" do
+		# 	it "has white color if :white is given" do
+		# 		allow(@cell.cell_color(:white))
+		# 		expect(@cell.color).to eq :white
+		# 	end
+
+		# 	it "has black color if :black is given" do
+		# 		allow(@cell.cell_color(:black))
+		# 		expect(@cell.color).to eq :black
+		# 	end
+
+		# end
+
+		context "#cell_player" do
+			it "initiates empty player for empty cell" do
+				expect(@cell.player).to eq :empty
 			end
 
-			it "has black color if :black is given" do
-				allow(@cell.cell_color(:black))
-				expect(@cell.color).to eq :black
+			it "gives player_white for White Pawn 1" do
+				allow(@cell.value = :WP1)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
 			end
 
+			it "gives player_white for White Pawn 2" do
+				allow(@cell.value =:WP2)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Pawn 3" do
+				allow(@cell.value = :WP3)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Pawn 4" do
+				allow(@cell.value = :WP4)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Pawn 5" do
+				allow(@cell.value = :WP5)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Pawn 6" do
+				allow(@cell.value = :WP6)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Pawn 7" do
+				allow(@cell.value = :WP7)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Pawn 8" do
+				allow(@cell.value = :WP8)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Rook 1" do
+				allow(@cell.value = :WR1)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Knight 1" do
+				allow(@cell.value = :WK1)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Bishop 1" do
+				allow(@cell.value = :WB1)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White King" do
+				allow(@cell.value = :WG)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Queen" do
+				allow(@cell.value = :WQ)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Bishop 2" do
+				allow(@cell.value = :WB2)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Knight 2" do
+				allow(@cell.value = :WK2)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_white for White Rook 2" do
+				allow(@cell.value = :WR2)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_white
+			end
+
+			it "gives player_black for Black Pawn 1" do
+				allow(@cell.value = :BP1)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Pawn 2" do
+				allow(@cell.value =:BP2)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Pawn 3" do
+				allow(@cell.value = :BP3)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Pawn 4" do
+				allow(@cell.value = :BP4)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Pawn 5" do
+				allow(@cell.value = :BP5)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Pawn 6" do
+				allow(@cell.value = :BP6)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Pawn 7" do
+				allow(@cell.value = :BP7)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Pawn 8" do
+				allow(@cell.value = :BP8)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Rook 1" do
+				allow(@cell.value = :BR1)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Knight 1" do
+				allow(@cell.value = :BK1)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Bishop 1" do
+				allow(@cell.value = :BB1)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black King" do
+				allow(@cell.value = :BG)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Queen" do
+				allow(@cell.value = :BQ)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Bishop 2" do
+				allow(@cell.value = :BB2)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Knight 2" do
+				allow(@cell.value = :BK2)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives player_black for Black Rook 2" do
+				allow(@cell.value = :BR2)
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :player_black
+			end
+
+			it "gives empty for an empty cell" do
+				allow(@cell.cell_player)
+				expect(@cell.player).to eq :empty
+			end
 		end
 
 	end
