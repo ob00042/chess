907b36bd41198759aa9d7040a02ba0841063c785
ob00042 <ob00042@gmail.com>
2016-09-16 18:17:19 +0300

    Delete unnecessary files

--- chess/lib/chess.rb
+++ chess/lib/chess.rb
@@ -6,7 +6,6 @@ end
 
 require_relative "./chess/cell.rb"
 require_relative "./chess/board.rb"
-require_relative "./chess/piece.rb"
 require_relative "./chess/game.rb"
 lib_path = File.expand_path(File.dirname(__FILE__))
 Dir[lib_path + "/chess/**/*.rb"].each { |file| require file }
